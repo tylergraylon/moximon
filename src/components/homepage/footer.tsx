c5ce4b1c8f9f11dfeaeb1e76f32c140f29a6fae6
victor Bello <victor.bello@tizeti.com>
2024-01-04 12:40:13 +0100

    changes

--- src/components/homepage/footer.tsx
+++ src/components/homepage/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="mx-8 md:mx-12 text-sm rounded-none flex flex-col justify-center items-center py-5 space-x-3 opacity-60">
+        <footer className="mx-8 md:mx-12 text-xs sm:text-sm rounded-none flex flex-col justify-center items-center py-5 space-x-3 opacity-60">
 
             <div>
                 &copy; {new Date().getFullYear()} XMAX ALL RIGHTS RESERVED
