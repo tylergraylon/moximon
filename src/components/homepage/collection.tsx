3606844fd69105f403b3859ab3558b794bf24c42
victor Bello <victor.bello@tizeti.com>
2024-01-05 05:46:53 +0100

    changes

--- src/components/homepage/collection.tsx
+++ src/components/homepage/collection.tsx
@@ -4,6 +4,7 @@ import { fetcherAPI } from "@/utils/utils"
 import useSWR from "swr"
 import { AxiosResponse } from "axios"
 import Image from "next/image"
+import { formatNumberToK } from "@/utils/utils"
 
 enum DATETOGGLE {
     oneh = '1h',
@@ -20,11 +21,31 @@ export default function Collection() {
             fetcherAPI, {
             revalidateOnFocus: false,
         })
+    const { data: volumeData, isLoading: volumeLoading } =
+        useSWR<AxiosResponse<{ volume: number, volume_change: number }>>(`https://api.opencnft.io/2/market/metrics?time_range=24h`,
+            fetcherAPI, {
+            revalidateOnFocus: false,
+        })
 
 
     const handleTimeChange = (e: BaseSyntheticEvent) => {
         setTimeRange(e.target.value)
     }
+
+    const volumeChange = (color: string, change: string | number) => (
+        <div className={`flex items-center space-x-2`}>
+            <span>
+                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12.75 5.25L4.5 13.5" stroke={`${color}`} stroke-width="1.5" stroke-linecap="round" />
+                    <path d="M8.25 4.5H12.75C13.1035 4.5 13.2803 4.5 13.3902 4.60984C13.5 4.71967 13.5 4.89645 13.5 5.25V9.75" stroke="#25DD81" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+            </span>
+
+            <span style={{ color: color }}>
+                {change}
+            </span>
+        </div>
+    )
     return (
         <section className="pt-8 mt-14 px-6 md:px-12">
 
@@ -41,42 +62,66 @@ export default function Collection() {
                 </h1>
             </div>
 
-            <div className="flex justify-between mt-6">
+            <div className="flex flex-col md:flex-row gap-4 justify-between mt-7 md:mt-14">
 
-                <div>
+                <div className="flex items-center justify-between bg-[#1C1938] p-3 order-last md:order-none text-xs md:text-base">
+
+                    <p className="text-[#8E8E8E]">
+                        Total 24h Volume:
+                    </p>
+
+                    <div className="flex items-center space-x-3 ml-4">
+                        <span>
+                            {volumeLoading || !volumeData ? '...' : (
+                                formatNumberToK(volumeData.data.volume)
+                            )}
+
+                        </span>
+
+                        {
+                            volumeLoading || !volumeData ? '...' :
+                                typeof volumeData.data.volume_change === 'number' && Math.sign(volumeData.data.volume_change) === 1 ? (
+                                    volumeChange('#25DD81', volumeData.data.volume_change.toFixed(2))
+                                ) : typeof volumeData.data.volume_change === 'number' && Math.sign(volumeData.data.volume_change) === -1 ? (
+                                    volumeChange('#EE4540', volumeData.data.volume_change.toFixed(2))
+                                ) : typeof volumeData.data.volume_change === 'number' && Math.sign(volumeData.data.volume_change) === 0 ? (
+                                    volumeChange('#8E8E8E', volumeData.data.volume_change.toFixed(2))
+                                ) : volumeChange('#8E8E8E', 'n/a')
+                        }
+                    </div>
 
                 </div>
 
-                <div className="tabs tabs-boxed gap-1 text-xs">
+                <div className="tabs tabs-boxed gap-1 text-xs w-full sm:w-fit rounded-none justify-evenly">
                     <input
                         type="radio" id="tab-13" name="tab-5" onChange={(e) => handleTimeChange(e)}
                         className="tab-toggle" value={DATETOGGLE.oneh}
                     //  checked={timeRange === DATETOGGLE.oneh} 
                     />
-                    <label htmlFor="tab-13" className={`tab text-xs ${timeRange === DATETOGGLE.oneh && 'bg-[#0000FF]'}`}>1h</label>
+                    <label htmlFor="tab-13" className={`tab text-xs ${timeRange === DATETOGGLE.oneh && 'bg-[#0000FF] hover:!bg-[#0000FF]'}`}>1h</label>
 
                     <input
                         type="radio" id="tab-14" name="tab-5" onChange={(e) => handleTimeChange(e)}
                         className="tab-toggle" value={DATETOGGLE.twentyfourh}
                     //  checked={timeRange === DATETOGGLE.twentyfourh} 
                     />
-                    <label htmlFor="tab-14" className={`tab text-xs ${timeRange === DATETOGGLE.twentyfourh && 'bg-[#0000FF]'}`}>24h</label>
+                    <label htmlFor="tab-14" className={`tab text-xs ${timeRange === DATETOGGLE.twentyfourh && '!bg-[#0000FF] hover:!bg-[#0000FF]'}`}>24h</label>
 
                     <input
                         type="radio" id="tab-15" name="tab-5" onChange={(e) => handleTimeChange(e)}
                         className="tab-toggle" value={DATETOGGLE.sevend} />
-                    <label htmlFor="tab-15" className={`tab ${timeRange === DATETOGGLE.sevend && 'bg-[#0000FF]'}`}>7d</label>
+                    <label htmlFor="tab-15" className={`tab ${timeRange === DATETOGGLE.sevend && 'bg-[#0000FF] hover:!bg-[#0000FF]'}`}>7d</label>
 
                     <input
                         type="radio" id="tab-4" name="tab-5" onChange={(e) => handleTimeChange(e)}
                         className="tab-toggle bg-[#0000FF]" value={DATETOGGLE.all} />
-                    <label htmlFor="tab-4" className={`tab ${timeRange === DATETOGGLE.all && 'bg-[#0000FF]'}`}>All</label>
+                    <label htmlFor="tab-4" className={`tab ${timeRange === DATETOGGLE.all && 'bg-[#0000FF] hover:!bg-[#0000FF]'}`}>All</label>
                 </div>
 
             </div>
 
 
-            <div className="my-10">
+            <div className="my-8 md:my-14">
 
                 {
                     isLoading || !data?.data ? (
