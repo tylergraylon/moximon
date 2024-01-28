2b595e7ecc5710ef04a015103759fefd954edb97
victor Bello <victor.bello@tizeti.com>
2024-01-28 16:40:00 +0100

    calma

--- src/components/presale/mint-box.tsx
+++ src/components/presale/mint-box.tsx
@@ -336,7 +336,8 @@ export default memo(function MintBox({
                             <span className="text-xs text-[#00FFFF]">
                                 {
                                     limit === 500 && assets && (
-                                        `${assets.filter((item) => (item.policyId === xmaxMintCardPolicyId)).length} Mint Cards`
+                                        `${assets.filter((item) => (item.policyId === xmaxMintCardPolicyId)).length} Mint Card${assets.filter((item) => (item.policyId === xmaxMintCardPolicyId)).length > 1 && 's'
+                                        }`
 
                                     )
                                 }
